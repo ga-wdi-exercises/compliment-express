6740bc51c1b40fcdbf49292337f077af3cc0fcd0
Becky Beauchamp <beckybeauchamp1@gmail.com>
2016-02-03 16:50:50 -0500

    Initial Commit

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 var express = require("express")
-var app = express()
+var app = express();
+var bodyParser = require('body-parser');
 var compliments = require("./models/compliment")
-var colors = require("./models/color")
+var colors = require("./models/color");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.set("view engine","hbs")
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/public"));
 
-app.get("/", function( req, res ){
+function getRandomCompliments(module){
+  var randomIndex = Math.floor((Math.random() * module.length));
+  var randomInstance = module[randomIndex];
+  return randomInstance;
+}
+
+app.get("/", function(req, res){
+  var randomCompliment = getRandomCompliments(compliments);
+  var randomColor = getRandomCompliments(colors);
+  res.render("index.hbs", {compliment: randomCompliment, color: randomColor});
+  console.log(randomCompliment);
  //display a generic greeting and a randomly chosen compliment. Th    e background color of the app should be randomized as well
-})
+});
+
+app.get("/:name", function(req, res){
+  var randomCompliment = getRandomCompliments(compliments);
+  var randomColor = getRandomCompliments(colors);
+  var name = req.params.name;
+  console.log(name);
+  res.render("name.hbs",{compliment: randomCompliment, color: randomColor, name: name});
+});
+
+app.post("/:name/new", function(req, res){
+  console.log("requessssst" + req);
+  console.log("requessssst" + res);
+  res.json();
+});
+
 
 app.listen(3000, function(){
   console.log("app listening at http://localhost:3000/")
-})
+});
