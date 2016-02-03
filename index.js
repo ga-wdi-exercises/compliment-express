57d6245dcdf64c9006a73674000b01a421559d05
Becky Beauchamp <beckybeauchamp1@gmail.com>
2016-02-03 18:08:56 -0500

    Adds Level 3 Solution

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express = require("express")
+var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
 var compliments = require("./models/compliment")
@@ -7,7 +7,7 @@ var colors = require("./models/color");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.set("view engine","hbs")
+app.set("view engine","hbs");
 app.use(express.static(__dirname + "/public"));
 
 function getRandomCompliments(module){
@@ -20,25 +20,21 @@ app.get("/", function(req, res){
   var randomCompliment = getRandomCompliments(compliments);
   var randomColor = getRandomCompliments(colors);
   res.render("index.hbs", {compliment: randomCompliment, color: randomColor});
-  console.log(randomCompliment);
- //display a generic greeting and a randomly chosen compliment. Th    e background color of the app should be randomized as well
 });
 
 app.get("/:name", function(req, res){
   var randomCompliment = getRandomCompliments(compliments);
   var randomColor = getRandomCompliments(colors);
   var name = req.params.name;
-  console.log(name);
   res.render("name.hbs",{compliment: randomCompliment, color: randomColor, name: name});
 });
 
 app.post("/:name/new", function(req, res){
-  console.log("requessssst" + req);
-  console.log("requessssst" + res);
-  res.json();
+  compliments.push(req.body.name);
+  var name = req.params.name;
+  res.redirect('/' + name);
 });
 
-
 app.listen(3000, function(){
-  console.log("app listening at http://localhost:3000/")
+  console.log("app listening at http://localhost:3000/");
 });
