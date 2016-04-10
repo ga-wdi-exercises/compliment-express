032347e5ddd294da42cbcac10f8f42c12a3922c3
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-04-10 17:43:02 -0400

    app is up and running - home page

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var hbs = require("express-handlebars");
+
+var app = express();
+
+app.set("view engine", "hbs");
+app.engine(".hbs", hbs({
+  extname: ".hbs",
+  partialsDir: "views/",
+  layoutsDir: "views/",
+  defaultLayout: "layout-main"
+}));
+app.use("/public", express.static("public"));
+
+app.get("/", function(req, res){
+  res.render("app-welcome");
+});
+
+app.listen(3005, function(){
+  console.log("IT'S ALIIIVE");
+});
