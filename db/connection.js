3f9299aec71be0671d6140ef23de77fd7ec7e023
Matthew Stevens <matthew.lee.stevens@gmail.com>
2016-04-11 14:03:54 -0400

    require models

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,10 @@
 var seedData = require("./seeds.json");
+
+var compliments = require("../models/compliment.js");
+var colors = require("../models/color.js");
+
+
+
 module.exports = {
   compliments: seedData
 };
