f2a23b3de5719e6ea4675f1a2c35c4cd0996df7e
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-04-12 03:03:18 -0400

    added codefor random function in index.js and created models file. Could not get json seeds to randomize - hardcoded compliments - please advise

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express     = require("express");
 var hbs         = require("express-handlebars");
 var db          = require("./db/connection");
+var compliments = require("./models/compliments")
 
 var app         = express();
 
@@ -14,20 +15,20 @@ app.engine(".hbs",hbs({
 
 app.use("/assets", express.static("public"));
 
-// function getRandomLove(compliments){
-//   var loveIndex = Math.floor((Math.random()* compliments.length));
-//   var loveInstance = compliments[LoveIndex];
-//   return loveInstance;
-//
-// }
+function getRandomCompliments(module){
+  var randomIndex = Math.floor((Math.random() * module.length));
+  var randomInstance = module[randomIndex];
+  return randomInstance;
+}
 
 app.get("/", function(req, res){
   res.render("welcome-compliment");
 });
 
 app.get("/compliments", function(req,res){
-  // var randomLove = getRandomLove(compliments);
+  var randomCompliment = getRandomCompliments(compliments);
   res.render("compliments",{
+    compliment: randomCompliment,
     compliments: db.compliments
   });
 });
---
+++ models/compliments.js
@@ -0,0 +1,7 @@
+
+module.exports = [
+    "All the Scots say you're worth kilting for...",
+    "Ewe're the kind of thing people can really count on!",
+    "Ewe look so much better than you smell.",
+    "The whole flocking crew has it so baaaaaaaaaad for ewe!"
+]
