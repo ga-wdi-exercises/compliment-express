8509c5a992dfa41b7b5c755dc3c827836de71ece
Ryan Krolick <rkrolick@gmail.com>
2016-04-12 17:57:34 -0400

    now displays random compliment, and supports /:name

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ var express = require("express");
 var hbs = require("express-handlebars");
 var app = express();
 
+var compliments = [
+  "Your instructors love you",
+  "High five = ^5",
+  "Is it Ruby Tuesday yet?",
+  "It's almost beer o'clock",
+  "The Force is strong with you"
+]
+
+var colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+
 
 app.set("view engine", "hbs");
 app.engine(".hbs", hbs({
@@ -13,9 +23,25 @@ app.engine(".hbs", hbs({
 app.use("/assets", express.static("public"));
 
 app.get("/", function(req, res){
-  res.render("index",{test:"test"});
+  res.render("index",{
+    compliment: randEntry(compliments),
+    color: randEntry(colors)
+  });
+});
+
+app.get("/:name", function(req, res){
+  res.render("index",{
+    compliment: randEntry(compliments),
+    color: randEntry(colors),
+    name: req.params.name
+  });
 });
 
 app.listen(3001, function(){
   console.log("It's aliiive!");
 });
+
+function randEntry(ary){
+  var i = Math.floor(Math.random() * ary.length);
+  return ary[i];
+}
