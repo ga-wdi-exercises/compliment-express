7c1d44e1f65168806c2c9ac7f275d4408b961f9e
Clarissa Stark <clarissa.stark@gmail.com>
2016-04-12 17:47:17 -0400

    added express.static and css

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.engine(".hbs", hbs({
   defaultLayout:  "layout-main"
 }));
 
+app.use("/assets", express.static("public"));
+
 app.get("/", function(req, res){
   res.render("layout-main");
 });
