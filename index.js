26d1b69462505b89edc0201b6987a3c1a057ae05
Patrick Moran <patrick_moran@mac.com>
2016-04-11 22:18:40 -0400

    comment changed on button click

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ var compliments = require("./models/compliment");
 var colors = require("./models/color");
 
 app.set("view engine","hbs");
-app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static("public"));
 
 app.get("/", function( req, res ){
- //display a generic greeting and a randomly chosen compliment. Th    e background color of the app should be randomized as well
- res.render("index", {
-   compliment: compliments[0]
- });
+  //display a generic greeting and a randomly chosen compliment. Th    e background color of the app should be randomized as well
+
+  // create random index
+  var compRandomIndex = Math.floor(Math.random() * compliments.length);
+  var colRandomIndex = Math.floor(Math.random() * colors.length);
+  // on button click, change compliment
+
+  res.render("index", {
+    compliment: compliments[compRandomIndex],
+    color: colors[colRandomIndex]
+  });
 });
 
 app.listen(3001, function(){
-  console.log("app listening at http://localhost:3000/");
+  console.log("app listening at http://localhost:3001/");
 });
