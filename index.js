6508b7f7eff376ecb5c7c017d233cd80a4f9657c
Woo Yun <hh747hh@gmail.com>
2016-04-12 05:58:43 -0400

    initial commit, save express, add views

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var hds     = require("express-handlebars");
+var app = express();
+
+app.set("view engine", "hbs");
+app.engine(".hbs", hbs({
+  extname:    ".hbs",
+  partialsDir: "views/",
+  layoutsDir: "views/",
+  defaultLayout: "layout-main"
+}));
+
+app.get("/", function(req, res){
+  res.render("app-welcome");
+});
+
+app.listen(3001, function(){
+  console.log("It's aliiive");
+});
