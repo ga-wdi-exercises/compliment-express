f48b0243fda95c652bc4a7b782936049616ae078
Patrick Moran <patrick_moran@mac.com>
2016-04-11 21:54:54 -0400

    page can display comment[0]

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-var express = require("express")
-var app = express()
-var compliments = require("./models/compliment")
-var colors = require("./models/color")
+var express = require("express");
+var app = express();
+var compliments = require("./models/compliment");
+var colors = require("./models/color");
 
-app.set("view engine","hbs")
-app.use(express.static(__dirname + "/public"))
+app.set("view engine","hbs");
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", function( req, res ){
  //display a generic greeting and a randomly chosen compliment. Th    e background color of the app should be randomized as well
-})
+ res.render("index", {
+   compliment: compliments[0]
+ });
+});
 
-app.listen(3000, function(){
-  console.log("app listening at http://localhost:3000/")
-})
+app.listen(3001, function(){
+  console.log("app listening at http://localhost:3000/");
+});
