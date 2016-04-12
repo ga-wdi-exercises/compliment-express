27d3ea0d9a76e27983c21d63e6c8e7a9279a4aee
Woo Yun <hh747hh@gmail.com>
2016-04-12 06:32:23 -0400

    add index, show routes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var hds     = require("express-handlebars");
+var db      = require("./db/connection");
 var app = express();
 
 app.set("view engine", "hbs");
@@ -10,9 +11,26 @@ app.engine(".hbs", hbs({
   defaultLayout: "layout-main"
 }));
 
+app.use("/compliments", express.static("public"));
 app.get("/", function(req, res){
   res.render("app-welcome");
 });
+
+app.get("/compliments", function(req, res){
+  res.render("compliments-index",{
+candidates: db.candidates
+  });
+});
+
+app.get("/compliments/:name", function(req, res){
+  var data = {
+    name: req.params.name
+  }
+  res.render("compliments-show", {
+    compliments: data
+  });
+});
+
 
 app.listen(3001, function(){
   console.log("It's aliiive");
