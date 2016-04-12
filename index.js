7ca6de488137cde6aa68b84d1dd8cbf0f96c17f5
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-04-12 02:44:40 -0400

    updated terrible sheep puns

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express     = require("express");
 var hbs         = require("express-handlebars");
 var db          = require("./db/connection");
 
-var app = express();
+var app         = express();
 
 app.set("view engine", "hbs");
 app.engine(".hbs",hbs({
@@ -14,11 +14,19 @@ app.engine(".hbs",hbs({
 
 app.use("/assets", express.static("public"));
 
+// function getRandomLove(compliments){
+//   var loveIndex = Math.floor((Math.random()* compliments.length));
+//   var loveInstance = compliments[LoveIndex];
+//   return loveInstance;
+//
+// }
+
 app.get("/", function(req, res){
   res.render("welcome-compliment");
 });
 
 app.get("/compliments", function(req,res){
+  // var randomLove = getRandomLove(compliments);
   res.render("compliments",{
     compliments: db.compliments
   });
