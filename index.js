7e9ee48da4d18e4d3119e06d20c7c55ce831657b
Ryan Krolick <rkrolick@gmail.com>
2016-04-12 16:05:44 -0400

    basic webapp created

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var hbs = require("express-handlebars");
+var app = express();
+
+
+app.set("view engine", "hbs");
+app.engine(".hbs", hbs({
+  extname: ".hbs",
+  partialsDir: "views/",
+  layoutsDir: "views/",
+  defaultLayout: "layout-main"
+}));
+app.use("/assets", express.static("public"));
+
+app.get("/", function(req, res){
+  res.render("index",{test:"test"});
+});
+
+app.listen(3001, function(){
+  console.log("It's aliiive!");
+});
