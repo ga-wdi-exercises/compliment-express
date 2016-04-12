891107db3f2d7f45d369e59a1c754cead5f18ac5
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-04-11 22:48:49 -0400

    add code to index.js to make sure express connection is there

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 var express = require("express");
 
 var app = express();
+
+app.get("/", function(req, res){
+  res.send("Hello, world!");
+})
+
+app.listen(3001, function(){
+  console.log("It's Aliiiive!");
+});
