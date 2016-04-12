c7045557ef8e843c227ebb96992ff40c787aa05f
Toni Langley <chibia2@gmail.com>
2016-04-12 14:13:08 -0400

    required express & handlebars

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var hbs = require("express-handlebars");
+
+var app = express();
+
+app.set("view engine", "hbs");
+
+app.engine(".hbs", hbs({
+  extname: ".hbs",
+  partialsDir: "views/",
+  layoutsDir: "views/",
+  defaultLayout: "layout-main"
+}));
+
+app.get("/", function(req, res){
+  res.render("app-welcome");
+});
+
+app.listen(3001, function(){
+  console.log("It's aliiiive!");
+});
