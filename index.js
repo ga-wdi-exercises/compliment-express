4cd4b94921edafb12da51170d14a8a99ddf35cfc
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-04-12 21:33:25 -0400

    displays data from seeds file

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var hbs = require("express-handlebars");
+var db = require("./db/connection");
+
+var app = express();
+
+app.set("view engine", "hbs");
+app.engine(".hbs", hbs({
+  extname: "hbs",
+  partialsDir: "views/",
+  layoutsDir: "views/",
+  defaultLayout: "layout-main"
+}));
+
+app.use("/assets", express.static("public"));
+
+
+app.get("/", function(req, res){
+  res.render("app-index", {
+    compilments: db.compliments
+  });
+});
+
+app.listen(3001, function(req, res){
+  console.log("it's aliiive!");
+});
