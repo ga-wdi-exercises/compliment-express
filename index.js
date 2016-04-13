91b7c379a4c34b5d94f4ff8d4202bc40b2c237c9
Anissa Jones <anissajones10@gmail.com>
2016-04-13 08:36:46 -0400

    Adds list of compliments.

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+compliments = [
+  "Your instructors love you",
+"Your smile is contagious.",
+"You look great today.",
+"You're a smart cookie.",
+"I bet you make babies smile.",
+"You have impeccable manners.",
+"I like your style.",
+"You have the best laugh.",
+"I appreciate you.",
+"You are the most perfect you there is.",
+"You are enough.",
+"You're strong.",
+"Your perspective is refreshing.",
+"You're an awesome friend.",
+"You light up the room.",
+"You deserve a hug right now.",
+"You should be proud of yourself.",
+"You're more helpful than you realize.",
+"The Force is strong with you"
+];
+
+colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"];
