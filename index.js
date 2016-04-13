bd2cc74b93929466fe4cc7b9020471f3858860c7
Keith Prifte <kprift@gmail.com>
2016-04-13 14:25:16 -0400

    adds show page and form and redirect, adding compliments not fully working

--- index.js
+++ index.js
@@ -1,20 +1,63 @@
 var express = require("express")
+var hbs     = require("express-handlebars")
+var params  = require("body-parser")
 var app     = express()
 
-compliments = [
-  "You don't suck",
-  "You're one of thecoolest people here, in this room.",
-  "You'll definately get a job. Eventually...",
-  "I don't care what every says about you, I still like you"
+var compliments = [
+  "Your instructors love you",
+  "High five = ^5",
+  "Is it Ruby Tuesday yet?",
+  "It's almost beer o'clock",
+  "The Force is strong with you"
 ]
 
-randomCompliment = compliments[Math.floor(Math.random() * compliments.length)]
-console.log(randomCompliment)
+colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+
+function getRandomCompliments(compliments){
+  var randomComplimentIndex = Math.floor(Math.random() * compliments.length)
+  return compliments[randomComplimentIndex]
+}
+
+function getRandomColor(colors){
+  var randomColorIndex = Math.floor(Math.random() * colors.length)
+  return colors[randomColorIndex]
+}
+
+
+app.set("view engine", "hbs");
+app.engine(".hbs", hbs({
+  extname: ".hbs",
+  partialsDir: "views/",
+  layoutsDir: "views/",
+  defaultLayout: "layout-main"
+}));
 
 app.get("/", function(req, res){
-  res.send(randomCompliment);
+  var randomCompliment = getRandomCompliments(compliments);
+  var randomColor = getRandomColor(colors)
+  res.render("index", {
+    compliment: randomCompliment,
+    color: randomColor
+  });
 });
 
-app.listen(3001, function(){
-  console.log("It's aliiiiveee")
+app.get("/:name", function(req, res){
+  var randomCompliment = getRandomCompliments(compliments);
+  var randomColor = getRandomColor(colors)
+  res.render("show", {
+    name: req.params.name,
+    compliment: randomCompliment,
+    color: randomColor
+  })
 })
+
+app.post("/:name/new", function(req, res){
+  compliments.push(params.compliment)
+  var name = req.params.name
+  res.redirect("/" + name )
+})
+
+
+app.listen(3001, function(){
+  console.log("I'm alive")
+});
