25964bfa8e7503c4cdcfc8f0da78df62405eb75c
Patrick Moran <patrick_moran@mac.com>
2016-04-13 17:34:28 -0400

    added level2: personalized page, and Level3: create compliments

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var express = require("express");
 var app = express();
 var compliments = require("./models/compliment");
 var colors = require("./models/color");
+var bodyParser = require("body-parser");
 
 app.set("view engine","hbs");
 app.use("/public", express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function( req, res ){
   //display a generic greeting and a randomly chosen compliment. Th    e background color of the app should be randomized as well
@@ -19,6 +21,25 @@ app.get("/", function( req, res ){
     color: colors[colRandomIndex]
   });
 });
+
+app.post("/", function(req, res) {
+  compliments.push(req.body.compliment);
+  res.redirect("/");
+});
+
+app.get("/:name", function(req, res) {
+  // create random index
+  var compRandomIndex = Math.floor(Math.random() * compliments.length);
+  var colRandomIndex = Math.floor(Math.random() * colors.length);
+  // on button click, change compliment
+
+  res.render("show", {
+    compliment: compliments[compRandomIndex],
+    color: colors[colRandomIndex],
+    name: req.params.name
+  });
+});
+
 
 app.listen(3001, function(){
   console.log("app listening at http://localhost:3001/");
