ccc0a0bdddd608d99a21894621c873e21ed4c8d4
Andrew Smith-Mui <arsm800@gmail.com>
2016-04-13 19:37:53 -0400

    Install JSON and sync to nodemon.

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res) {
+  res.send("Hello!");
+});
+
+
+app.listen(3001, function() {
+  console.log("Working!");
+});
