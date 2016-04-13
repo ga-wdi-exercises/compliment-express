674dd932e18d4ab10db063ee0ee51e0d98e4e9a0
Bipin Neupane <icomp2@hotmail.com>
2016-04-13 19:25:25 -0400

    adds css styling and javascript link

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ app.engine(".hbs", hbs({
   partialsDir: "views/",
   layoutsDir: "views/",
   defaultLayout: "layout-main"
+  // layout main is saying that it is being displayed globally, pointed at layout-main.hbs file
 }));
-// app-welcome is point to the app-welcome.hbs file
+// assets is looking in the public folder
+app.use("/assets", express.static("public"));
+// app-welcome is pointing to the app-welcome.hbs file and is being rendered inside layout-main due to the body handlebars
 app.get("/", function(req, res){
   res.render("app-welcome");
 });
