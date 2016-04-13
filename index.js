d64c315975d18adcd25b48d0cbda2fff19a2b261
Anissa Jones <anissajones10@gmail.com>
2016-04-13 19:49:39 -0400

    Adds reandom function. Supplies function to view. Calls color in view.

--- index.js
+++ index.js
@@ -15,17 +15,27 @@ app.engine(".hbs", hbs({
 app.use("/assets", express.static("public"));
 
 function RandomCompliment(mycomplimentsdata){
-  var randomindex = Math.floor((Math.random() * mycomplimentsdata.length))
-  console.log(randomindex);
+  var randomindex = Math.floor((Math.random() * mycomplimentsdata.length));
   finalcompliment = mycomplimentsdata[randomindex];
 
   return finalcompliment;
 }
 
+
+function RandomColor(){
+  var color = "#";
+  letternums = ["FFBF00", "0080FF","01DF3A","FF0080"];
+  color = color + letternums[Math.floor(Math.random() * letternums.length)];
+  return color;
+}
+
 app.get("/", function(req, res){
   finalcompliment = RandomCompliment(comdata);
+  finalcolor = RandomColor();
+  console.log(finalcolor);
   res.render("app-compliment", {
-    compliments: finalcompliment["ment"]
+    compliments: finalcompliment["ment"],
+    color: finalcolor
   });
 });
 
@@ -33,6 +43,4 @@ app.listen(3001, function(){
   console.log("It's aliiive!");
 });
 
-
-
-colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"];
+RandomColor();
