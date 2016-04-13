6960f35902e59842c18a22262d4347d2667424d3
YOUR FULL NAME <farishta.haider@gmail.com>
2016-04-13 19:14:57 -0400

    framework done. Customizing to assignment needs

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var hbs = require("express-handlebars");
+var db = require("./db/connection");
 
 var app = express();
 
+app.set("port", process.env.PORT || 3001);
 app.set("view engine", "hbs");
 app.engine(".hbs", hbs({
   extname:        ".hbs",
@@ -12,10 +14,30 @@ app.engine(".hbs", hbs({
 }));
 app.use("/assets", express.static("public"));
 
-app.get("/", function(){
+app.get("/", function(req, res){
   res.render("app-welcome");
 });
 
-app.listen(3001, function(){
+app.get("/compliments", function(req, res){
+  res.render("compliments-index", {
+    compliments: db.compliments
+  });
+});
+
+app.get("/compliments/:name", function(){
+  var desiredName = req.params.name;
+  var complimentOutput;
+  db.compliments.forEach(function(compliment){
+    if (desiredName === compliment.name){
+      complimentOutput = compliment;
+    }
+  });
+
+  res.render("compliments-show", {
+    compliment: complimentOutput
+  });
+});
+
+app.listen(app.get("port"), function(){
   console.log("Look at me, I work!!!!");
 });
