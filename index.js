bd39453824618d26d856cf522ef323e9dc7b7ac3
Charles <charlesthwang@gmail.com>
2016-04-13 14:15:04 -0400

    added express.static, styles.css, and app.js

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ var hbs = require("express-handlebars");
 
 var app = express();
 
+app.set("view engine", "hbs");
+app.engine(".hbs", hbs({
+  extname: ".hbs",
+  partialDir: "views/",
+  layoutsDir: "views/",
+  defaultLayout: "layout-main"
+}));
+
+app.use("/assets", express.static("public"));
+
 app.get("/", function(req, res){
-  res.send("Hello, World!");
+  res.render("app-welcome");
 });
 
 app.listen(3001, function(){
