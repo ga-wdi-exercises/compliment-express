681caa4dfe9d6b585309d99d7e1fa66cb08a67f6
YOUR FULL NAME <farishta.haider@gmail.com>
2016-04-13 17:52:18 -0400

    configured handlebars

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var express = require("express");
 var hbs = require("express-handlebars");
+
 var app = express();
 
+app.set("view engine", "hbs");
+app.engine(".hbs", hbs({
+  extname:        ".hbs",
+  partialDir:     "views/",
+  layoutsDir:     "views/",
+  defaultLayout:  "layout-main"
+}));
+
+
 app.get("/", function(){
-  res.send("Hello, World!");
+  res.render("app-welcome");
 });
 
 app.listen(3001, function(){
