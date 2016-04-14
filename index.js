6479cfbc0d1bb381423babcc0b511002b25492d7
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-04-13 21:03:16 -0400

    "able to click and link to other page."

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express =require("express");
+var hbs =require("express-handlebars");
 var app = express();
 app.set("view engine", "hbs");
 app.get("/", express.static("public"));
@@ -10,7 +11,9 @@ app.get("/", function(req, res){
 app.listen(3001, function(){
   console.log("It's aliiive!");
 });
+function myCompliments(){
 
+}
 compliments = [
   "Your instructors love you",
   "High five = ^5",
