718c1d4c71b7ff29d7a2c23b3627948457d53605
caseyagil <caseyagil@gmail.com>
2016-04-13 21:17:43 -0400

    added views and layout

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ var express = require("express");
 var hbs = require("express-handlebars");
 var app = express();
 
+app.set("view engine", "hbs");
+ app.engine(".hbs", hbs({
+   extname: ".hbs",
+   partialsDir: "views/",
+   layoutsDir: "views/",
+   defaultLayout: "layout-main"
+ }));
+
+
 app.get("/", function(req, res){
   res.send("Here's a compliment");
 });
 
+res.render("app-welcome");
 app.listen(3001, function(){
   console.log("It's aliive!");
 });
