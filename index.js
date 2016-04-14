9e0d6e02cdb295872c5051f47afdfc364e8ad1f0
caseyagil <caseyagil@gmail.com>
2016-04-13 21:32:10 -0400

    reorderd files and corrected paths

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var express = require("express");
 var hbs = require("express-handlebars");
+var db = require("./db/connection");
 var app = express();
 
+var compliments = require("./models/compliment");
+var colors = require("./models/color");
+
 app.set("view engine", "hbs");
- app.engine(".hbs", hbs({
-   extname: ".hbs",
-   partialsDir: "views/",
-   layoutsDir: "views/",
-   defaultLayout: "layout-main"
- }));
+function giveCompliments(module){
+   var randomIndex = Math.floor((Math.random() * module.length));
+   var randomInstance = module[randomIndex];
+   return randomInstance;
+
 app.use("/assets", express.static("public"));
 
 app.get("/", function(req, res){
@@ -16,11 +19,11 @@ app.get("/", function(req, res){
 });
 
 app.get("/compliments", function(req, res){
-   res.render("compliments-index", {
-     numCompliments: 10
-   });
- });
- 
+  res.render("compliments-index", {
+    numCompliments: 10
+  });
+});
+
 
 
 app.listen(3001, function(){
