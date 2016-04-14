4004ab3236bea714a0e9c4517cfdbc28dfaa4e0a
Melissa Miller <lis.d.miller@gmail.com>
2016-04-13 21:31:01 -0400

    Adds additional styling and actual compliments

--- models/compliments.js
+++ models/compliments.js
@@ -1,6 +1,9 @@
 module.exports = [
-  "Compliment 1",
-  "Compliment 2",
-  "Compliment 3",
-  "Compliment 4"
+  "Have a boo-tiful day",
+  "I scare about you",
+  "I'm dead...excited to be your friend",
+  "My you are looking wonderfully translucent today",
+  "You're looking extra spooky today",
+  "How do you keep your ghoulish figure?",
+  "I'm envious of your afterlifestyle"
 ];
