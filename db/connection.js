1976b5503ed0f91c91024e6643c7455b16f27c92
Bipin Neupane <icomp2@hotmail.com>
2016-04-13 20:11:07 -0400

    db connection and notes

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,5 @@
 var seedData = require("./seeds.json");
 module.exports = {
   compliments: seedData
-  // this compliments property is being accessed in index.js at compliments: db.compliments
+  // this compliments is looking at seeds.json and the compliments property is being accessed in index.js at compliments: db.compliments
 };
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var hbs = require("express-handlebars");
 var db = require("./db/connection");
+// db variable is being mapped to the directory and is being accessed below at     compliments: db.compliments
+
 var app = express();
 
 
@@ -22,6 +24,8 @@ app.get("/", function(req, res){
 app.get("/compliments", function(req, res){
   res.render("compliments-index", {
     compliments: db.compliments
+    // compliments could be named anything. it is looking at db variable and compliments is looking at compliments in connection.js file   compliments: seedData
+
   });
 });
 
