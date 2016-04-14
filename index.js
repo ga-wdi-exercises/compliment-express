4aaa3e52b6eb895c08d0fedcb450f33c8c3eb76b
Woo Yun <hh747hh@gmail.com>
2016-04-14 05:11:13 -0400

    index set up,npm install express

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
 var express = require("express");
-var hds     = require("express-handlebars");
-var db      = require("./db/connection");
+
 var app = express();
 
-app.set("view engine", "hbs");
-app.engine(".hbs", hbs({
-  extname:    ".hbs",
-  partialsDir: "views/",
-  layoutsDir: "views/",
-  defaultLayout: "layout-main"
-}));
-
-app.use("/compliments", express.static("public"));
 app.get("/", function(req, res){
-  res.render("app-welcome");
-});
-
-app.get("/compliments", function(req, res){
-  res.render("compliments-index",{
-candidates: db.candidates
-  });
-});
-
-app.get("/compliments/:name", function(req, res){
-  var data = {
-    name: req.params.name
-  }
-  res.render("compliments-show", {
-    compliments: data
-  });
+  res.send("Hello, World!");
 });
 
 
