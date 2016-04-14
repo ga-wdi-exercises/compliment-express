10c3823741a627f9d2d4bae4caa2908f292cd3b0
winishasmith <winishasmith@gmail.com>
2016-04-14 00:18:23 -0400

    added views

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ var express = require("express");
 var hbs = require("express-handlebars");
 var app = express();
 
+app.set("view engine", "hbs");
+
+compliments = [
+  "Your instructors love you",
+  "High five = ^5",
+  "Is it Ruby Tuesday yet?",
+  "It's almost beer o'clock",
+  "The Force is strong with you"
+]
+
 app.get("/", function(req,res){
-  res.send("Hello World");
+  res.send(compliments);
 });
+
 
 app.listen(3001, function(){
   console.log("Houston we have lift off!");
