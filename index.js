14a5040a434677c8ef4f7ab11690bad821922e47
Lauren C. Kennedy <laurenckennedy@gmail.com>
2016-05-15 16:36:18 -0400

    level 1

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-var express = require("express");
-var hbs     = require("express-handlebars");
+var express    = require("express");
+var hbs        = require("express-handlebars");
+var data       = require("./data/compliment")
 
-var app     = express();
+var app        = express();
+
+var functions  = {
+  randomColor : function(){
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+
+    return "rgb(" + r + "," + g + "," + b + ")";
+  },
+  randomIndex: function(){
+    return Math.floor(Math.random() * data.length)
+  }
+};
 
 app.set("view engine", "hbs");
 app.engine(".hbs", hbs({
   extname:        ".hbs",
   partialsDir:    "views/",
   layoutsDir:     "views/",
-  defaultLayout:  "layout-main"
+  defaultLayout:  "layout-main",
 }))
 
 app.get("/", function(req, res){
-  res.render("app-index")
+  res.render("app-index",{
+    backgroundColor: functions.randomColor(),
+    compliments: data[functions.randomIndex()]
+  })
 })
 
 app.listen(3001, function(){
