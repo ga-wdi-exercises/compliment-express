8233ecab90dcbaa84e394032e39b21728e52b93a
Hewan Abreha <henebys@gmail.com>
2016-05-15 14:09:21 -0400

    added code and my connection is aliiiive

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 var express = require("express");
-var express = require("express-handlebars")
+// var express = require("express-handlebars");
 
 var app = express();
+
+app.get("/", function(req, res){
+  res.send("Hello, world!");
+});
+
+app.listen(3001, function(){
+  console.log("It's Aliiiiiive!");
+});
