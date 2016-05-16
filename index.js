5157b38d336610bfcd004c97c5139c3c0fde308b
Hewan Abreha <henebys@gmail.com>
2016-05-15 20:22:32 -0400

    configured handlebars and eddited app-welcome

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var express = require("express");
 var express = require("express-handlebars");
+var db      = require("./db/connection");
 
 var app = express();
 
+
+app.set("view engine", "hbs");
+app.engine(".hbs", hbs({
+  extname:        ".hbs",
+  partialDir:     "views/",
+  layoutsDir:     "views/",
+  defaultLayout:  "layout-main"
+}));
+
 app.get("/compliment", function(req, res){
-  res.render("compliment-welcome");
+  res.render("app-welcome");
 });
+
 
 app.listen(3001, function(){
   console.log("It's Aliiiiiive!");
