d94c78522e9aa5c266f53f2c59a44a9912faf277
Martin Terskin <martinas.terskin@gmail.com>
2016-05-16 17:29:41 -0400

    first bonus, complete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var hbs = require("express-handlebars");
 var db = require("./db/connection");
+var parser = require("body-parser");
 
 var app = express();
 
@@ -13,19 +14,30 @@ app.engine(".hbs", hbs({
   defaultLayout: "layout-main"
 }));
 app.use("/assets", express.static("public"));
+app.use(parser.urlencoded({extended: true}));
+
 
 app.get("/", function(req, res){
-  function getRandomInt(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  var randomComplement = db.complements[getRandomInt(0,db.complements.length-1)];
-  var randomColor = db.colors[getRandomInt(0,db.colors.length-1)];
   res.render("app-welcome",{
-    randomComplement: randomComplement,
-    randomColor: randomColor
+    randomComplement: db.complements[getRandomInt(0,db.complements.length-1)],
+    randomColor: db.colors[getRandomInt(0,db.colors.length-1)]
+  });
+});
+
+app.post("/", function(req, res){
+  var name = req.body.first;
+  res.render("app-welcome",{
+    randomComplement: db.complements[getRandomInt(0,db.complements.length-1)],
+    randomColor: db.colors[getRandomInt(0,db.colors.length-1)],
+    name: name
   });
 });
 
 app.listen(3001, function(){
   console.log("It's aliiiiiiiiice!");
 });
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
