581ab2caba8e02cb20b4519e5b0d2286b5890d03
Zaneh Williams <zanehmw@gmail.com>
2016-05-16 10:12:07 -0400

    didn't know that this was homework, starting now

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var hbs = require("express-handlebars");
+var db = require("./db/connection");
+var app = express();
+
+app.set("view engine", "hbs");
+app.engine(".hbs", hbs({
+  extname: ".hbs",
+  partialsDir: "views/",
+  layoutsDir: "views/",
+  defaultLayout: "layout-main"
+}));
+
+app.use("/assets", express.static("public"));
+
+app.get("/", function(req, res) {
+  res.render ("app-welcome");
+});
