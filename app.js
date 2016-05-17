497136eddd931f8b4aa60e1f2ec678b3716165d6
Leland Jordon <lelandjordon@mac.com>
2016-05-17 09:15:46 -0400

    split compliments and colors into separate files

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-var express = require("express");
+var express = require("express");   // makes the app load the dependency Express
+var app = express();                // makes the app actually run Express
 var hbs = require("handlebars");
 var mongoose = require("./db/connection")
+
+app.get("/", function(req, res){     // directs the app to do something at the '/' path
+  res.send("dog");
+});
+
+app.listen(4444, function(){     // makes the app listen to port 4444 from the browser
+  console.log("it works");
+});
