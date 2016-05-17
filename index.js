9f7cf64c242f97a865c52e5bee1fedcfc3e0ede7
Zaneh Williams <zanehmw@gmail.com>
2016-05-17 09:58:44 -0400

    able to render compliments and color in h2 tag randomly through help of tylers lesson

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require("express");
 var hbs = require("express-handlebars");
-var db = require("./db/connection");
+// var db = require("./db/connection");
 var app = express();
+
+var colors = require("./models/colors.js")
+var compliments = require("./models/compliments.js")
 
 app.set("view engine", "hbs");
 app.engine(".hbs", hbs({
@@ -10,9 +13,21 @@ app.engine(".hbs", hbs({
   layoutsDir: "views/",
   defaultLayout: "layout-main"
 }));
-
+//
 app.use("/assets", express.static("public"));
 
+function randPick(array){
+  var pick = Math.floor(Math.random() * array.length);
+  return array[pick];
+}
+
 app.get("/", function(req, res) {
-  res.render ("app-welcome");
+  res.render("index.hbs", {
+    randColor: randPick(colors),
+   randCompli: randPick(compliments)
+  });
+});
+
+app.listen(3001, function() {
+  console.log("Its aliiivee");
 });
