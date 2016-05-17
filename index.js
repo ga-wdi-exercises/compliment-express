9f9ef80a97a0321ca104089f9af9a6c479cfba45
Matt <mvaden08@gmail.com>
2016-05-17 09:49:26 -0400

    created hbs, dadded functionality to index.js, created index views.

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-var express = require("express");
-var app = express();
+var express       = require("express");
+var app           = express();
+var hbs           = require('./express-handlebars')
+var colors        = require('./models/colors');
+var compliments   = require('/models/compliments')
+// npm i -S express
 
-app.listen(4000, function(){
 });
+
+function randomPick(arr) {
+  var pick = Math.floor(Math.random() = arr.length)
+}
+
+app.set('view engine', 'hbs')
+app.engine(',hbs', hbs(){
+  extname: '.hbs',
+  partials:
+})
+// handlebars are like ERBs in rails
 
 app.get("/", function(req, res){
-  res.send("Hello World");
+  // res.render('./views/index.hbs');
+    randColor: randPick(colors,
+    randCompli: randPick(compliments)
 });
+
+app.listen(4444, function(){
+
+// test by using node app.js
+// mkdir models/compliments.js
--- models/compliments.js
+++ models/compliments.js
@@ -1,9 +1,9 @@
-module.exports = {
-  scompliments = [
+var compliments = [
     "Your instructors love you",
     "High five = ^5",
     "Is it Ruby Tuesday yet?",
     "It's almost beer o'clock",
     "The Force is strong with you"
   ]
-}
+
+module.exports = compliments
