fe666123ede6ed76c8662f36848310140ec55bf9
Tyler Crosse <tylerscottcrosse@gmail.com>
2016-05-17 10:06:17 -0400

    Everything from morning-review

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express     = require('express');
 var app         = express();
+var hbs         = require('express-handlebars')
 var colors      = require('./models/colors')
 var compliments = require('./models/compliments')
 
@@ -8,8 +9,21 @@ function randPick(arr) {
   return arr[pick];
 }
 
+app.set('view engine', 'hbs');
+app.engine(".hbs", hbs({
+  extname:        ".hbs",
+  partialsDir:    "views/",
+  layoutsDir:     "views/",
+  defaultLayout:  "layout-main",
+}))
+
 app.get('/', function(req,res){
- res.send(randPick(compliments))
+  var randColor = randPick(colors)
+  res.render('index.hbs', {
+    pizza: "I love pizza",
+    randColor: randColor,
+    randCompli: randPick(compliments)
+  });
 });
 
 app.listen(4444, function(){
