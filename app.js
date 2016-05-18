2a23fbd52f53d0c5a6dc8a86919e1e031a08c246
Leland Jordon <lelandjordon@mac.com>
2016-05-18 00:32:19 -0400

    added randomizers and added randComp to index view.

--- app.js
+++ app.js
@@ -5,17 +5,27 @@ var colors      = require("./db/colors");            // retrieves arrays of colo
 var compliments = require("./db/compliments");    // compliments from these files
 // var mongoose    = require("./db/connection")
 
+function randPick(arr) {               // Randomly picks a whole integer based on the
+  var pick = Math.floor(Math.random() * arr.length);   // length of the array passed in.
+  return arr[pick];
+}
+
 app.set('view engine', 'hbs');
-app.engine('.hbs', hbs({        // configures Handlebars.  Throwing an error, though.
-  extname:        ".hbs",
+app.engine('.hbs', hbs({        // configures Handlebars.  Threw an error, though,
+  extname:        ".hbs",       // when I had required simply "handlebars" in line 3.
   partialsDir:    "views/",
-  layoutsDir:     "views/",
+  layoutsDir:     "views/",     // These tell the app where to find partial views and layouts.
   defaultLayout:  "layout-main.hbs",
 }))
 
 app.get("/", function(req, res){     // directs the app to do something at the '/' path
   // res.send("dog");                  // displays "dog" in the browser view.
-  res.render("index");
+  var randColor = randPick(colors)
+  var randComp  = randPick(compliments)
+  res.render("index", {
+    randColor: randColor,
+    randComp:  randComp
+  });
 });
 
 app.listen(4444, function(){     // makes the app listen to port 4444 from the browser
