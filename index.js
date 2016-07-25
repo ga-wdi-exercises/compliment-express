2486a06b36041843ec2470e582da90e607e4492f
Dave Corderman <dcordz@gmail.com>
2016-07-25 10:03:28 -0400

    did some shit, it all works now

--- index.js
+++ index.js
@@ -2,40 +2,31 @@ var express = require('express')
 var app = express()
 var compliments = require("./models/compliments")
 
-var colors = require("./models/colors.js")
 var comps = require("./models/comps.js")
 
 var comp_no_name = require("./controllers/compliment_no_name.js")
 
+var bodyParser = require("body-parser");
+
 // render views using handlebars, from npm install hbs --save
 app.set("view engine", "hbs")
+
+app.use(bodyParser.json()); //handles json post requests
+app.use(bodyParser.urlencoded({ extended: true })); // handles form submissions
 
 // use non-hbs items in the public directory
 app.use(express.static(__dirname +'/public'))
 
-function randoComp() {
-  return comps[Math.floor(Math.random() * comps.length)]
-}
-
-function randoColor() {
-  return colors[Math.floor(Math.random() * colors.length)]
-}
-
 // get the compliments.home method and set path / to the method
-app.get("/", function(req, res) {
-    res.render("index", {
-      comp: randoComp(),
-      col: randoColor()
-    })
-  })
+app.get("/", compliments.home)
 
-app.get("/:name", function(req, res) {
-  res.render("index", {
-    comp: randoComp(),
-    col: randoColor(),
-    name: req.params.name
-  })
+app.post("/", function(req, res){
+  comps.push(req.body.new_compliment)
+  res.redirect("/")
+  // redirects to app.get("/")?
 })
+
+app.get("/:name", compliments.name)
 
 // app.get("/:compliment", compliments.getCompliment)
 
--- models/compliments.js
+++ models/compliments.js
@@ -1,27 +1,31 @@
-// var comps = require("./comps.js")
-// var colors = require("./colors.js")
-//
-// module.exports = {
-//     // home method, called using app.get in index.js, render's hbs index view index.hbs and does function
-//
-//     home: function(req, res) {
-//
-//       res.render("index", {
-//         //why do I need "comps:", 2nd arg of res.render is options?
-//         comp: comps[Math.floor(Math.random() * comps.length)],
-//
-//         col: colors[Math.floor(Math.random() * colors.length)]
-//       })
-//     },
-//
-//     name: function(req, res) {
-//
-//       res.render("index", {
-//         name: req.params.name,
-//
-//         comp: comps[Math.floor(Math.random() * comps.length)],
-//
-//         col: colors[Math.floor(Math.random() * colors.length)]
-//       })
-//     }
-//   }
+// is this cool? setting global vars/requires in a controller?
+
+var comps = require("./comps.js")
+var colors = require("./colors.js")
+
+function randoCompAndColor() {
+  var comp = comps[Math.floor(Math.random() * comps.length)]
+  var col = colors[Math.floor(Math.random() * colors.length)]
+  return [comp, col]
+}
+
+module.exports = {
+    // home method, called using app.get in index.js, render's hbs index view index.hbs and does function
+
+    // why can't I create non-method functions in here? Because it's an object duh
+
+    home: function(req, res) {
+      res.render("index", {
+        comp: randoCompAndColor()[0],
+        col: randoCompAndColor()[1]
+      })
+    },
+
+    name: function(req, res) {
+      res.render("index", {
+        name: req.params.name,
+        comp: randoCompAndColor()[0],
+        col: randoCompAndColor()[1]
+      })
+    }
+  }
