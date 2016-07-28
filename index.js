f8de8621c574f3e9f73b266d401e7348bd452882
Thaisun Green <Tlgreen16@gmail.COM>
2016-07-27 21:18:19 -0400

    think its done

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var app = express();
-app.listen(8008);
-app.get('/', function(req, res){
-  res.send('hello world');
-});
-app.get("/compliment",function(req,res){
-  res.send("compliment");
-})
+var express = require("express")
+ var app = express()
+ var data = require("./data.js")
+ app.set("view engine", "hbs")
+ app.use(express.static("public"))
+ app.listen(8085)
+
+
+function pickRandom(item){
+   return item[Math.floor(item.length * Math.random())]
+ }
+
+ app.get("/", function(req, res){
+   res.render("index", {
+     compliment: pickRandom(data.compliments),
+     color: pickRandom(data.colors)
+   })
+ })
