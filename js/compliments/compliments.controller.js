7f9ed0cce42435c04c56afd5c8dd0c2548bd0e16
Kevin Golden <kwg@cs.net>
2016-08-01 18:24:23 -0400

    Turn it in

--- js/compliments/compliments.controller.js
+++ js/compliments/compliments.controller.js
@@ -1,11 +1,21 @@
 `use strict`;
 
 (function(){
-  var app = angular.module("compliments");
-  app.controller("complimentsController", ComplimentsController);
+  angular
+  .module("compliments")
+  .controller("ComplimentsController", [
+    ComplimentsController
+  ]);
 
   function ComplimentsController(){
     var vm = this;
+    var compNum = data[Math.floor(Math.random() * data.length)];
+    console.log(compNum);
+    var compDisplay = data[compNum];
   }
 
 }());
+
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.title = 'Top Sellers in Books';
+}]);
