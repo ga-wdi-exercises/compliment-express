8b3753ade32939b7002e76d36807b51174acf26a
Tyler Rosenberg <tyryanr@gmail.com>
2016-08-27 19:37:17 -0400

    actually forked repo this time and cloned. Copied original files to new forked repo copy.

---
+++ controllers/compliments.controller.js
@@ -0,0 +1,15 @@
+module.exports = {
+  compliments: [
+    "Your instructors love you",
+    "High five = ^5",
+    "Is it Ruby Tuesday yet?",
+    "It's almost beer o'clock",
+    "The Force is strong with you"
+  ],
+  colors: [
+    "#FFBF00",
+    "#0080FF",
+    "#01DF3A",
+    "#FF0080"
+  ]
+}
