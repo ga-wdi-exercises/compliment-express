bb4a0665ab7d6ec2393f74b6cbf0e26aba88a76d
Mike Flores <michaelprflores@gmail.com>
2016-11-04 14:31:24 -0400

    installed hbs as a dependency and set it as the view engine

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var express = require("express")
+var compliment-express = require("express")
 var app = express()
 
 app.listen(5000, () => {
   console.log("app listening on port 5000")
 })
+
+app.set("view engine", "hbs")
