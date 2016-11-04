b64a14dc8fc8942f2f7721f9ba949a3e8d93e111
Nick Oki <nick.oki@live.com>
2016-11-04 12:29:01 -0400

    Add index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js
+
+// Modules & dependencies
+var express = require("express")
+
+// Init app
+var app = express()
+
+
+
+// Server
+app.listen(4000, () => {
+  console.log("app listening on port 4000")
+})
