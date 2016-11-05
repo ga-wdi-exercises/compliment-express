1d4ca175a63f1d545ff269bc1a6d9f5335005f01
Michael Lai <mike24567@gmail.com>
2016-11-05 18:41:02 -0400

    initialized hello world in express

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require("express")
+var app = express()
+
+app.set("view engine","hbs")
+
+app.listen(8000, () => {
+  console.log("CONNECTED ON port 8000")
+})
+
+app.get("/", (req, res) => {
+  res.send("helloooooooo world")
+})
+
+
+
+
+
+
+
+let compliments = [
+  "Your instructors love you",
+  "High five = ^5",
+  "Is it Ruby Tuesday yet?",
+  "It's almost beer o'clock",
+  "The Force is strong with you"
+]
+
+let colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
