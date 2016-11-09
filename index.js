3d30a5fbea6e2c0a5ac8561e5ca1535a7697e518
RYANBROPHY <BROPHY2321@ME.COM>
2016-11-09 14:29:18 -0500

    added views

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var app = express();
+var hbs = require("hbs")
+var body-parser = require("body-parser")
 
 var compliments = [
   "Your instructors love you",
@@ -14,8 +16,17 @@ var colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
 app.set("view engine", "hbs")
 app.use(express.static("public"))
 
+function RandomCompliments(){
+    var randomComplimentIndex = Math.floor((Math.random() * module.length));
+  var randomInstance = module[randomComplimentIndex];
+  return randomInstance;
+}
+
 app.get("/", (req, res) => {
-  res.send(compliments)
+    var randomComp= RandomCompliments(compliments)
+    var randomColor = RandomCompliments(colors)
+    var name = req.params.name;
+    res.render("index.hbs", {compliment: randomComp, color: randomColor, name: name})
 })
 
 app.listen(3000, () => {
