7d7d36771ba2ce31e130ec80c398fc112af533cc
Mike Flores <michaelprflores@gmail.com>
2016-11-09 14:38:26 -0500

    setup files for views, compliments show as json

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ var bodyParser = require("body-parser")
 
 app.set("view engine", "hbs")
 
+
+var compliments = [
+  "Your instructors love you",
+  "High five = ^5",
+  "Is it Ruby Tuesday yet?",
+  "It's almost beer o'clock",
+  "The Force is strong with you"
+]
+
+var colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+
 app.listen(3000, () => {
-  console.log("app listening on port 3000")
+  console.log("3000")
+})
+
+app.get("/", (req, res) => {
+  res.send(compliments)
+  console.log("hey this works!")
 })
