4230fcdaf2e141cb2e2b44efa35304fc59cca6ca
Alexander <ajspottswood@gmail.com>
2016-11-09 22:50:04 -0500

    Level 2 complete

--- index.js
+++ index.js
@@ -7,19 +7,12 @@ var colors = require("./colors")
 app.set ("view engine", "hbs")
 app.use(express.static("./public"))
 
-app.get("/", (req,res) => {
+app.get("/:name?", (req,res) => {
   var compliment = compliments[Math.floor((Math.random() * compliments.length))]
   var color = colors[Math.floor((Math.random() * colors.length))]
-  res.render("index", {compliment, color})
+  var name = req.params.name? req.params.name : ''
+  res.render("index", {compliment, color, name})
 });
-
-
-
-
-
-
-
-
 
 app.listen(4000, () => {
   console.log("app listening on port 4000");
