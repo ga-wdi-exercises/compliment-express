6fb45bc58f83cc1d7ad0022f0a1f7a42fa0dd78d
Alexander <ajspottswood@gmail.com>
2016-11-09 23:07:10 -0500

    Level 3

--- index.js
+++ index.js
@@ -6,12 +6,19 @@ var colors = require("./colors")
 
 app.set ("view engine", "hbs")
 app.use(express.static("./public"))
-
+app.use(bodyParser.urlencoded({extended: true}))
 app.get("/:name?", (req,res) => {
   var compliment = compliments[Math.floor((Math.random() * compliments.length))]
   var color = colors[Math.floor((Math.random() * colors.length))]
   var name = req.params.name? req.params.name : ''
   res.render("index", {compliment, color, name})
+});
+
+app.post("/:name?", (req,res) => {
+  var name = req.params.name? req.params.name : ''
+  var newCompliment = req.body.compliment
+  compliments.push(newCompliment)
+  res.redirect('/' + name)
 });
 
 app.listen(4000, () => {
