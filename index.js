a91e45a94aab6eadecd2cfb0b01caeff4941fad6
Alexander <ajspottswood@gmail.com>
2016-11-09 22:37:52 -0500

    Level 1 complete

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser")
+var compliments = require("./compliments")
+var colors = require("./colors")
+
+app.set ("view engine", "hbs")
+app.use(express.static("./public"))
 
 app.get("/", (req,res) => {
-  res.render("Welcome!");
+  var compliment = compliments[Math.floor((Math.random() * compliments.length))]
+  var color = colors[Math.floor((Math.random() * colors.length))]
+  res.render("index", {compliment, color})
 });
+
+
+
+
+
+
+
+
 
 app.listen(4000, () => {
   console.log("app listening on port 4000");
