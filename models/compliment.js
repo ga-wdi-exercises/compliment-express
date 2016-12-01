92c00c2f019a0b2a123cfd5f7780e08ccfffb819
Kevin Herrera <1herrerake@gmail.com>
2016-11-30 19:27:23 -0500

    inital commit

---
+++ models/compliment.js
@@ -0,0 +1,7 @@
+module.exports = [
+  "Every little thing is going to be alright!",
+  "You're Awesome.",
+  "You're perfect just the way you are.",
+  "Go Crazy!",
+  "The force is strong with you."
+]
