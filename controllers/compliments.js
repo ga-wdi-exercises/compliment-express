4d2058a5ddffc5964b88c075351808d75113fc1e
Nat Carter <n.goldman.carter@gmail.com>
2017-03-18 19:38:18 -0400

    refactored with controller and views

---
+++ controllers/compliments.js
@@ -0,0 +1,22 @@
+const compliments = [
+  "Surely, someone is stupider than you.",
+  "Turn that frown upside down ... Now wipe that smirk off your face.",
+  "Really? That shirt with those pants?",
+  "Thanks for visiting my site, but don't you have anything better to do?",
+  "Sit up straight; your posture is crap."
+]
+
+function randomElement(arr) {
+  let len = arr.length
+  let index = Math.floor(Math.random() * len)
+  return arr[index]
+}
+
+module.exports = {
+  sample(req, res) {
+    let compliment = randomElement(compliments)
+    res.render("index", {
+      compliment
+    })
+  }
+}
--- index.js
+++ index.js
@@ -1,27 +1,11 @@
 const express = require("express")
 const app = express()
+const compliments = require("./controllers/compliments.js")
 
-const compliments = [
-  "Your instructors love you",
-  "High five = ^5",
-  "Is it Ruby Tuesday yet?",
-  "It's almost beer o'clock",
-  "The Force is strong with you"
-]
-
-function sample(A) {
-  let n = A.length
-  let i = Math.floor(Math.random()*n)
-  return A[i]
-  return i
-}
+app.set("view engine", "hbs")
 
 app.listen(4000, _ => {
   console.log("App listening on port 4000")
 })
 
-app.get("/", (req, res) => {
-  let compliment = sample(compliments)
-  res.send(`<p>Hello!</p>
-            <p>${compliment}</p>`)
-})
+app.get("/", compliments.sample)
