41a9340075c8121ea897e8bea10e6950b70d30b2
Fiona <h.natapra@gmail.com>
2017-03-20 18:37:50 -0400

    level 3 - user submitted compliments

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
-const compliments = require("./compliments.js")
+// const compliments = require("./compliments.js")
 
+var compliments = [
+  "Your instructors love you!!" ,
+  "High five!!",
+  "Is it Ruby Tuesday yet?",
+  "It's almost beer o'clock!!",
+  "The Force is strong with you",
+  "I bet you sweat glitter",
+  "Unicorns!!"
+]
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.set("view engine", "hbs")
 app.use(express.static(__dirname + '/public'))
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 
  // randomized compliment & color
-app.get("/", compliments.index)
+app.get("/", function(req, res) {
+  let compliment = compliments[Math.floor(Math.random()*compliments.length)]
+  let colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+  let color = colors[Math.floor(Math.random()*colors.length)]
+  res.render('index', { compliments, compliment, colors, color})
+})
+
+ // personalized compliment
+app.get("/:name", function(req, res) {
+  let compliment = compliments[Math.floor(Math.random()*compliments.length)]
+  let colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+  let color = colors[Math.floor(Math.random()*colors.length)]
+  let name = `${req.params.name}`
+  res.render('index', { compliments, compliment, colors, color, name})
+})
 
 // add a compliment
-app.post("/", (req,res) => {
- res.render("index", {
-   new_compliment: req.body.new_compliment,
- })
+app.post("/:name", function(req,res){
+let name = `${req.params.name}`
+ compliments.push(req.body.new_compliment);
+ console.log(compliments)
+ res.redirect("/" + name)
 })
- // personalized compliment
-app.get("/:name", compliments.index)
 
 
 app.listen(4000, () => {
