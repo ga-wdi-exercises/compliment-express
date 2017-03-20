07fd6b13f62630575c61dd79cadff7704414b5d6
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-03-20 13:51:17 -0400

    views linked

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.listen(4000, () => {
-  console.log("app listening on port 4000");
-})
+app.set("view engine", "hbs")
+app.use(express.static(__dirname + "/public"))
+
 
 app.get("/", (req, res) => {
-  res.send("Hello World"); 
+  res.send("Hello World");
+})
+
+app.listen(4000, () => {
+  console.log("app listening on port 4000");
 })
