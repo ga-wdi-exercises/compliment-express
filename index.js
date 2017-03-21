9c5949c8d0d46c4bc981f4dcfed76626256d6aba
Chris Wightman <wightman_christopher@bah.com>
2017-03-21 18:45:08 -0400

    Mostly working. Can pull name into views. Need to get random compliments and apply a random color.

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+
+const compliments = [
+  "Your instructors love you",
+  "High five = ^5",
+  "Is it Ruby Tuesday yet?",
+  "It's almost beer o'clock",
+  "The Force is strong with you"
+]
+
+const colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+
 
 app.set("view engine", "hbs")
 
-app.listen(4000, () => {
-  console.log("Listening for requests on port 4000");
-});
+app.use(bodyParser.json()); //handles json post requests
+app.use(bodyParser.urlencoded({ extended: true })); // handles form submissions
 
-app.use(express.static(_dirname + '/public'))
-
-app.get("/:name", (req, res) => {
-  let name = req.params.name
-  res.render("index", "What's up " + {name} "?") ;
-});
+// app.use(express.static(_dirname + '/public'))
 
 app.get("/", (req,res) => {
-  res.send("Your Random Compliment")
+  res.render("welcome")
+});
+
+app.get("/:name", (req, res) => {
+  let name = req.params.name || "stranger";
+  res.render("index") ;
+});
+
+app.post("/", (req, res) => {
+  res.render("index", {
+    name: req.body.name,
+    compliment: compliments.random
+  })
+})
+
+app.listen(4000, () => {
+  console.log("Listening for requests on port 4000");
 });
