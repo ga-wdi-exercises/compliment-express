891b43b9646cbeaf50078ee6dc3b5dbaad3ac7f5
Bedig Galladian <bgalladian@gmail.com>
2017-03-21 16:41:59 -0400

    finished

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-var compliments = [
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.set("view engine", "hbs");
+
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+let compliments = [
   "Your instructors love you",
   "High five = ^5",
   "Is it Ruby Tuesday yet?",
@@ -6,25 +17,26 @@ var compliments = [
   "The Force is strong with you"
 ]
 
-var colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
 
-const express = require("express");
-const app = express();
-app.set("view engine", "hbs")
+let colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
 
 
-app.get("/", (req, res) => {
+app.get("/:name?", (req, res) => {
 
   let compliment = compliments[Math.floor(Math.random() * compliments.length)];
   let color = colors[Math.floor(Math.random() * colors.length)];
-  res.render("index", {compliment, color})
+  let name = req.params.name
+  res.render("index", {compliment, color, name})
+
 });
 
-app.get('/:name', (req, res) => {
-  res.send(`Hello ${req.params.name}`)
-})
+app.post("/:name/new", (req, res) => {
+  compliments.push(req.body.newCompliment);
+  let name = req.params.name;
+  res.redirect(`/${name}`);
+});
 
 
 app.listen(4000, () => {
-  console.log("app listening on port 4000");
+  console.log('listening on port 4000')
 });
