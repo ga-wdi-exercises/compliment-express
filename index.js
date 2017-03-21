52cb34d99e1e897c2974dbf8cf6dd4f0205c839b
Habtamu Abere <habbuya@gmail.com>
2017-03-20 23:58:51 -0600

    views and console.log randoms

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
 
+let compliments = [
+  "Your instructors love you",
+  "High five = ^5",
+  "Is it Ruby Tuesday yet?",
+  "It's almost beer o'clock",
+  "The Force is strong with you"
+]
+compliment = compliments[Math.floor(Math.random() * compliments.length)];
+console.log(compliment)
+
+let colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+
+color = colors[Math.floor(Math.random() * colors.length)];
+
+console.log(color)
 
 app.get("/", (req, res) => {
   res.send("Hello World");
