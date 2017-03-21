49b77ad95af06538f3ba1948b2a92a6cf8981b05
Bedig Galladian <bgalladian@gmail.com>
2017-03-21 16:14:46 -0400

    got compliment and rando color working

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-compliments = [
+var compliments = [
   "Your instructors love you",
   "High five = ^5",
   "Is it Ruby Tuesday yet?",
@@ -6,15 +6,19 @@ compliments = [
   "The Force is strong with you"
 ]
 
-colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+var colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
 
 const express = require("express");
 const app = express();
+app.set("view engine", "hbs")
 
 
-app.get('/', (req, res) => {
-  res.send('Hello bedig,')
-})
+app.get("/", (req, res) => {
+
+  let compliment = compliments[Math.floor(Math.random() * compliments.length)];
+  let color = colors[Math.floor(Math.random() * colors.length)];
+  res.render("index", {compliment, color})
+});
 
 app.get('/:name', (req, res) => {
   res.send(`Hello ${req.params.name}`)
