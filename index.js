c9b0ae5c8fb32d691bfe6ebfb148ba13f6ebe03e
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-03-21 18:00:29 -0400

    it's working mvp

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const parser = require("body-parser");
+const app = express();
 
 app.set("view engine", "hbs");
 app.use(express.static(__dirname + "public"));
+app.use(parser.json());
+app.use(parser.json({extended: true}));
 
 let compliments = [
   "Your instructors love you",
@@ -25,6 +28,11 @@ app.get("/:name?", (req, res) => {
   let color = colors[Math.floor(Math.random()* colors.length)];
   res.render("index", {name, compliment, color});
 })
+
+app.post("/", (req, res) => {
+  compliments.push(req.body);
+    res.redirect("/")
+  })
 
 app.listen(4000, () => {
   console.log("app is listening on port 4000")
