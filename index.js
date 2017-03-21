345b2d4c2e78bbed59122bb027f365e16ff74575
Chris Wightman <wightman_christopher@bah.com>
2017-03-21 19:17:03 -0400

    Random comments are working. Need to work on random color.Might peek at the solution a bit

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const compliments = require("./models/compliment");
+const colors = require("./models/color");
 
 
-const compliments = [
-  "Your instructors love you",
-  "High five = ^5",
-  "Is it Ruby Tuesday yet?",
-  "It's almost beer o'clock",
-  "The Force is strong with you"
-]
-
-const colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
-
-
-app.set("view engine", "hbs")
 
 app.use(bodyParser.json()); //handles json post requests
 app.use(bodyParser.urlencoded({ extended: true })); // handles form submissions
 
+app.set("view engine", "hbs")//not sure if it matters if this has to be below the other app.use or not.
 // app.use(express.static(_dirname + '/public'))
 
+function getRandomCompliment(module){
+  var randomIndex = Math.floor((Math.random() * module.length));
+  var randomInstance = module[randomIndex];
+  return randomInstance;
+}
+
+
 app.get("/", (req,res) => {
-  res.render("welcome")
+  var randomCompliment = getRandomCompliment(compliments);
+  var randomColor = getRandomCompliment(colors);
+  res.render("welcome", {compliment: randomCompliment, color: randomColor});
 });
 
 app.get("/:name", (req, res) => {
   let name = req.params.name || "stranger";
-  res.render("index") ;
+  res.render("index", {compliment: randomCompliment, color: randomColor}) ;
 });
 
 app.post("/", (req, res) => {
+  var name = req.params.name
+  var randomCompliment = getRandomCompliment(compliments);
+  var randomColor = getRandomCompliment(colors);
   res.render("index", {
     name: req.body.name,
-    compliment: compliments.random
+    compliment: randomCompliment,
+    color: randomColor
   })
 })
 
