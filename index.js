646c201eb7fb42dd5278de25c2b867753cd7308c
Gurpreet Gill <gill_gurpreet2@bah.com>
2017-03-22 14:43:49 -0400

    all files added

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const parser = require("body-parser")
 const app = express();
-const compliments = require("./models/compliments.js");
-const colors = require("./models/color.js");
+
+app.set("view engine", "hbs")
+app.use(express.static(__dirname + '/public'))
+app.use(parser.json())
+app.use(parser.urlencoded({extended: true}))
+
+
+let compliments = [
+  "Your instructors love you",
+  "High five = ^5",
+  "Is it Ruby Tuesday yet?",
+  "It's almost beer o'clock",
+  "The Force is strong with you"
+]
+
+var colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+
 
 app.listen(3001, () =>{
   console.log("express started on port 3001")
