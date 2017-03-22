3ee5c8e434b92a566fb9d15590bae1ba6a34894c
Aaron <andrews_aaron@bah.com>
2017-03-22 13:55:06 -0400

    product is working

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser')
-const compliments = require('./models/compliments.js')
-const colors = require('./models/color.js')
+const bodyParser = require("body-parser");
+const compliments = require("./models/compliments.js");
+const colors = require("./models/color.js");
 
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.set("view engine", "hbs");
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/pubilc"));
+
 
 function getRandomCompliments(module){
   var randomIndex = Math.floor((Math.random() * module.length));
   var randomInstance = module[randomIndex];
-  return randomIndex
+  return randomInstance;
 }
 
-app.get("/", function(req, res){
-  
-})
+app.get("/", function(req,res){
+  var randomCompliment = getRandomCompliments(compliments);
+  var randomColor = getRandomCompliments(colors);
+  res.render("index.hbs", {compliment: randomCompliment, color: randomColor});
+});
+
+app.get("/:name", function(req, res){
+  var randomCompliment = getRandomCompliments(compliments);
+  var randomColor = getRandomCompliments(colors);
+  var name = req.params.name;
+  res.render("name.hbs", {compliment: randomCompliment, color: randomColor, name: name});
+});
+
+app.post("/:name/new", function(req, res){
+  compliments.push(req.body.name);
+  var name = req.params.name;
+  res.redirect("/" + name);
+});
+
+app.listen(4000, function(){
+  console.log("App listening on port 4000");
+});
