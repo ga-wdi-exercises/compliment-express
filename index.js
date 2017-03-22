bddd949747fa5f0c9f1b48397696315f2daabd73
Erica Norby <ericanorby@gmail.com>
2017-03-22 12:16:23 -0400

    added body-parser as dependency and form to create compliment

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express")
+const parser = require("body-parser")
 const app = express()
 
 app.listen(4000)
 
 app.set("view engine", "hbs")
 app.use(express.static(__dirname + '/public'))
+app.use(parser.json())
+app.use(parser.urlencoded({extended: true}))
 
 let compliments = [
   "Your instructors love you",
@@ -22,6 +25,11 @@ app.get("/", (req,res) => {
   let compliment = compliments[Math.floor(Math.random() * compliments.length)]
   let color = colors[Math.floor(Math.random() * colors.length)]
   res.render("index", {compliment, color})
+})
+
+app.post("/", (req,res) => {
+  compliments.push(req.body.compliment)
+  res.redirect("/")
 })
 
 app.get("/:name", (req,res) => {
