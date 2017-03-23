9b823eca25a2901713451526a81e2e2bc369287a
shapiro_benjamin@bah.com <benjaminshapiro@C1MS50ZQH3QK.fios-router.home>
2017-03-22 23:12:43 -0400

    Complete Level 2

---
+++ models/color.js
@@ -0,0 +1,1 @@
+module.exports = ["red", "blue", "green", "yellow", "lemonchiffon"];
---
+++ models/compliment.js
@@ -0,0 +1,8 @@
+module.exports = [
+  "All the instructors love you!",
+  "You are a strong, confident woman who does not need to smoke.",
+  "You know that 'I would walk 500 miles' song?  It was about you!",
+  "You have a voice that could make a wolverine purr, and suits so fine they make Sinatra look like a hobo.",
+  "You are worthy of picking up Mjolnir!",
+  "You're all, like, good at coding, and stuff."
+];
