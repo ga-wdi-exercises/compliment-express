e55a7d1811b634ddc55fba98a5aacbabebba59f3
Gurpreet Gill <gill_gurpreet2@bah.com>
2017-03-23 06:52:39 -0400

    last commit

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-const express = require("express");
-const parser = require("body-parser")
-const app = express();
+const express = require('express')
+const app = express()
+const parser = require('body-parser')
 
-app.set("view engine", "hbs")
-app.use(express.static(__dirname + '/public'))
 app.use(parser.json())
 app.use(parser.urlencoded({extended: true}))
 
-
 let compliments = [
-  "Your instructors love you",
-  "High five = ^5",
-  "Is it Ruby Tuesday yet?",
-  "It's almost beer o'clock",
-  "The Force is strong with you"
-]
+   "Your instructors love you",
+   "High five = ^5",
+   "Is it Ruby Tuesday yet?",
+   "It's almost beer o'clock",
+   "The Force is strong with you"
+ ]
+let colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
 
-var colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+app.set("view engine", "hbs")
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req,res) => {
+   let compliment = compliments[Math.floor(Math.random() * compliments.length)]
+   let color = colors[Math.floor(Math.random() * colors.length)]
+   res.render('index', {compliment,color})
+})
+
+app.get('/:name', (req,res) => {
+  let name = req.params.name
+  let compliment = compliments[Math.floor(Math.random() * compliments.length)]
+  let color = colors[Math.floor(Math.random() * colors.length)]
+  res.render('index', {name,compliment,color})
+
+})
 
 
 app.listen(3001, () =>{
