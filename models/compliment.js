0009d30f3d2608ec7d513074d5de602bb54f245f
Adam Hoff <swimmerdude79@comcast.net>
2017-05-22 15:32:03 -0400

    changed styling

--- models/compliment.js
+++ models/compliment.js
@@ -1,7 +1,11 @@
 module.exports = [
-  "Your instructors love you",
-  "High five = ^5",
+  "Your instructors love you.",
+  "High five = ^5.",
   "Is it Ruby Tuesday yet?",
-  "It's almost beer o'clock",
-  "The Force is strong with you"
+  "It's almost beer o'clock.",
+  "The Force is strong with you.",
+  "You're the life of the party.",
+  "You are the favorite child in the family.",
+  "Everyone admires your work.",
+  "You never slur or get nervous when speaking in public."
 ]
