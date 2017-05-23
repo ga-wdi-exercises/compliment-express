a0c049ce00de212b513c47714552f63192e5f8cf
Jonathan Wright <jonwright.va@gmail.com>
2017-05-22 21:42:52 -0400

    completes level 3

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const app = express();
+const bodyParser = require("body-parser")
 
 app.set("view engine", "hbs")
 app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true}))
 
 let compliments = [
 	  "Your instructors love you",
@@ -25,6 +28,16 @@ app.get('/:name', (req, res) => {
 	let color = colors[Math.floor(Math.random()*colors.length)]
 	let name = req.params.name
 	res.render("index", {compliment, color, name})
+})
+
+app.post('/', (req, res) => {
+	if (compliments[compliments.length - 1] != req.body.userCompliment) {
+		compliments.push(req.body.userCompliment)
+	}
+
+	let compliment = compliments[Math.floor(Math.random()*compliments.length)]
+	let color = colors[Math.floor(Math.random()*colors.length)]
+	res.render("index", {compliment, color})
 })
 
 app.listen(4000, () => {
