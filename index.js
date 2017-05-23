8a66dfd629e9b9878a31f005a4c4a7a3e2e7010e
Jonathan Wright <jonwright.va@gmail.com>
2017-05-22 21:19:42 -0400

    completes level 2

--- index.js
+++ index.js
@@ -4,22 +4,27 @@ const app = express();
 app.set("view engine", "hbs")
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => {
-	let compliments = [
+let compliments = [
 	  "Your instructors love you",
 	  "High five = ^5",
 	  "Is it Ruby Tuesday yet?",
 	  "It's almost beer o'clock",
 	  "The Force is strong with you"
 	]
-	let colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+let colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+
+app.get('/', (req, res) => {
 	let compliment = compliments[Math.floor(Math.random()*compliments.length)]
 	let color = colors[Math.floor(Math.random()*colors.length)]
 	res.render("index", {compliment, color})
 })
 
 app.get('/:name', (req, res) => {
-	res.send(`Hello ${req.params.name}`)
+	
+	let compliment = compliments[Math.floor(Math.random()*compliments.length)]
+	let color = colors[Math.floor(Math.random()*colors.length)]
+	let name = req.params.name
+	res.render("index", {compliment, color, name})
 })
 
 app.listen(4000, () => {
