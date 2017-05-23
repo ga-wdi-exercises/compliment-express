e59e66ae995320d43b4faaad2adea28da92ad9bb
Jonathan Wright <jonwright.va@gmail.com>
2017-05-22 21:13:25 -0400

    completes level 1

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require("express")
 const app = express();
 
+app.set("view engine", "hbs")
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', (req, res) => {
-	res.send("hello")
+	let compliments = [
+	  "Your instructors love you",
+	  "High five = ^5",
+	  "Is it Ruby Tuesday yet?",
+	  "It's almost beer o'clock",
+	  "The Force is strong with you"
+	]
+	let colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+	let compliment = compliments[Math.floor(Math.random()*compliments.length)]
+	let color = colors[Math.floor(Math.random()*colors.length)]
+	res.render("index", {compliment, color})
+})
+
+app.get('/:name', (req, res) => {
+	res.send(`Hello ${req.params.name}`)
 })
 
 app.listen(4000, () => {
 	console.log("whuddup")
 })
+
+
+
