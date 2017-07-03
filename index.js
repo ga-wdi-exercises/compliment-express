d1c5a99d604f8f22dd28dea9e849ad66ec2313f9
Tarik Kazanci <tarikkzc@gmail.com>
2017-07-03 17:12:32 -0400

    created app, created views folder

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+// const bodyParser = require("body-parser")
+
+// app.use(bodyParser.urlencoded({extended: true}))
+
+app.set("view engine", "hbs")
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World")
+})
+
+app.listen(4000, () => {
+  console.log("app listening on port 4000")
+})
