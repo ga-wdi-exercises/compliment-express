674eca074779bc4166e186e536bb7fe8d6849231
SONNY KEOBOUNPHANH <SKEOBOUNPHANH@GMAIL.COM>
2017-07-02 23:11:45 -0400

    level 3:user submitted compliments

--- controllers/complimentsController.js
+++ controllers/complimentsController.js
@@ -1,21 +1,22 @@
 module.exports = {
   index: (req, res) => {
-    compliments = [
-      "Your instructors love you",
-      "High five = ^5",
-      "Is it Ruby Tuesday yet?",
-      "It's almost beer o'clock",
-      "The Force is strong with you"
-    ]
-
-    colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
 
     const name = req.params.name
+    var newCompliment = req.body.newCompliment
+
+
+    if(typeof newCompliment !== "undefined")  {
+      compliments.push(newCompliment)
+      newCompliment = undefined;
+      // console.log(`set to undefined. ${newCompliment}`);
+    }
+
+    // console.log(compliments);
     const changeColor = Math.floor(Math.random() * colors.length)
     const changeCompliment = Math.floor(Math.random() * compliments.length)
 
     res.render("index", {
-      // playerName: req.body.playerName,
+      newCompliment: newCompliment,
       name: name,
       bgcolor: colors[changeColor],
       compliment: compliments[changeCompliment]
--- index.js
+++ index.js
@@ -5,10 +5,23 @@ const complimentsController = require("./controllers/complimentsController")
 
 app.set("view engine", "hbs")
 
+const bodyParser = require("body-parser")
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+compliments = [
+  "Your instructors love you",
+  "High five = ^5",
+  "Is it Ruby Tuesday yet?",
+  "It's almost beer o'clock",
+  "The Force is strong with you"
+]
+
+colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
 
 app.get("/:name?", complimentsController.index)
 
-
+app.post("/", complimentsController.index)
 
 app.listen(4000, () => {
   console.log("app listening on port 4000")
