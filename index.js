65a247ae473047d3368a68efd5f0d5b111c5b239
Tchase44 <tchase4544@gmail.com>
2017-07-04 14:14:06 -0400

    hacky waay to add colors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+
+// npm install --save express
+// npm install --save hbs
 const express = require("express")
 const app = express()
 
 let compliments = [
   "Your instructors love you",
   "High five = ^5",
-  "Is it Ruby Tuesday yet?",
+  "You're gonna destroy today!",
   "It's almost beer thirty",
   "The Force is strong with you"
 ]
@@ -14,10 +17,19 @@ function randomThing(array) {
 	return Math.floor(Math.random()*array.length)
 }
 
-app.listen(4000,()=>{
-	console.log("listening on port 4000")
+// app.listen(4000,()=>{
+// 	console.log("listening on port 4000")
+// })
+
+app.get("/",(req,res) => {
+	res.send(`<body style="background:${colors[randomThing(colors)]};" >
+				<h1>Greetings!</h1>
+				<h2>${compliments[randomThing(compliments)]}</h2>
+			</body>`)
 })
 app.get("/:name",(req,res) => {
-	res.send(`<h1>${req.params.name}, remember that...</h1>
-		<h2>${compliments[randomThing(compliments)]}</h2>`)
+	res.send(`<body style="background:${colors[randomThing(colors)]};" >
+				<h1>Hi ${req.params.name}!</h1>
+				<h2>${compliments[randomThing(compliments)]}</h2>
+			</body>`)
 })
