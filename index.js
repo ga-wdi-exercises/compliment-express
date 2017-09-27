d0bd3d79afb5df7970c953c9bc295cf06b01e851
Amarinder Pandher <amarinder_pandher@yahoo.com>
2017-09-27 15:53:51 -0400

    finished level 3

--- index.js
+++ index.js
@@ -5,22 +5,32 @@ var colors = require('./models/color.js');
 var compliments = require('./models/compliment.js')
 
 app.use(parser.urlencoded({extended:true}));
+
+//tells you what localhost to put in url
 app.set("port", process.env.PORT || 3000);
 app.set('view-engine', 'hbs');
 
+//the default page
 app.get("/", function (req, res){
-  var randColor = colors[Math.floor(Math.random()*colors.length)];
-  var randCompl = compliments[Math.floor(Math.random()*compliments.length)];
-  res.render("index.hbs", {compliment: randCompl, color: randColor});
+  var randColor = colors[Math.floor(Math.random()*colors.length)]; //randome generator
+  var randCompl = compliments[Math.floor(Math.random()*compliments.length)]; //random generator
+  res.render("index.hbs", {compliment: randCompl, color: randColor}); // setting declared variables to local variables(like this.)
 });
 
+//will happen if you put a name in the url
 app.get("/:name", function (req, res){
-  var randColor = colors[Math.floor(Math.random()*colors.length)];
-  var randCompl = compliments[Math.floor(Math.random()*compliments.length)];
+  var randColor = colors[Math.floor(Math.random()*colors.length)]; //random generator
+  var randCompl = compliments[Math.floor(Math.random()*compliments.length)]; //random generator
   var name = req.params.name;
-  res.render("name.hbs", {compliment: randCompl, color: randColor, name:name});
+  res.render("name.hbs", {compliment: randCompl, color: randColor, name:name}); //setting declared variables to local variables(like this.)
 });
 
+app.post("/:name/compliment", function(req, res){
+  compliments.push(req.body.name);
+  res.redirect('/' + req.params.name);
+});
+
+//in cli, will tell you 3000 is open, meaning nothing is wrong
 app.listen(app.get("port"), function(){
   console.log("3000 is open");
 });
