a869654b04e1cd9565d4748781cbe96f8b055d6f
Stella <sgordonzigel@gmail.com>
2017-12-05 13:27:11 -0500

    level two: personalized compliment

--- index.js
+++ index.js
@@ -31,3 +31,23 @@ app.get("/", (req, res) => {
     color: color
   })
 })
+
+app.get("/:name?", (req, res) => {
+  let compliments = [
+    "Your instructors love you",
+    "High five = ^5",
+    "Is it Ruby Tuesday yet?",
+    "It's almost beer o'clock",
+    "The Force is strong with you"
+  ]
+
+  let colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+
+  let compliment = (`Hey ${req.params.name},  ${(compliments[Math.floor(Math.random() * compliments.length)])}`)
+  let color = colors[Math.floor(Math.random() * colors.length)]
+
+  res.render("index", {
+    compliment: compliment,
+    color: color
+  })
+})
