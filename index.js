724e45dee59338ca513a1e3dc80a257f8e0cedd0
Sarah Yu <sarahyu0692@gmail.com>
2017-12-05 09:48:30 -0500

    user can submit compliments

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const hbs = require('hbs')
+const bodyParser = require('body-parser')
 
 const app = express()
 
 app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 compliments = [
 	'Your instructors love you',
@@ -16,10 +19,23 @@ compliments = [
 colors = ['#FFBF00', '#0080FF', '#01DF3A', '#FF0080']
 
 app.get('/:name?', (req, res) => {
-	let n = Math.floor(Math.random() * compliments.length)
-	let compliment = compliments[n]
+	let compliment = compliments[Math.floor(Math.random() * compliments.length)]
 	let bgColor = colors[Math.floor(Math.random() * colors.length)]
 	let name = req.params.name || 'WDI 19'
+
+	res.render('index', {
+		compliment: compliment,
+		bgColor: bgColor,
+		name: name
+	})
+})
+
+app.post('/:name?', (req, res) => {
+	let compliment = compliments[Math.floor(Math.random() * compliments.length)]
+	let bgColor = colors[Math.floor(Math.random() * colors.length)]
+	let name = req.params.name || 'WDI 19'
+
+	compliments.push(req.body.new_compliment)
 
 	res.render('index', {
 		compliment: compliment,
