08e9ead869b035ddc0f951ffd62284a3900e2748
Stella <sgordonzigel@gmail.com>
2017-12-05 14:22:01 -0500

    level 3: user submitted compliments

--- index.js
+++ index.js
@@ -5,23 +5,24 @@ const bodyParser = require('body-parser')
 const app = express ()
 
 app.set("view engine", "hbs")
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(3000, () => {
   console.log("app listening on port 3000")
 })
 
-app.get("/", (req, res) => {
-  let compliments = [
-    "Your instructors love you",
-    "High five = ^5",
-    "Is it Ruby Tuesday yet?",
-    "It's almost beer o'clock",
-    "The Force is strong with you"
-  ]
+let compliments = [
+  "Your instructors love you",
+  "High five = ^5",
+  "Is it Ruby Tuesday yet?",
+  "It's almost beer o'clock",
+  "The Force is strong with you"
+]
 
-  let colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+let colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+
+app.get("/", (req, res) => {
 
   let compliment = (`Hey WDI,  ${(compliments[Math.floor(Math.random() * compliments.length)])}`)
   let color = colors[Math.floor(Math.random() * colors.length)]
@@ -33,15 +34,6 @@ app.get("/", (req, res) => {
 })
 
 app.get("/:name?", (req, res) => {
-  let compliments = [
-    "Your instructors love you",
-    "High five = ^5",
-    "Is it Ruby Tuesday yet?",
-    "It's almost beer o'clock",
-    "The Force is strong with you"
-  ]
-
-  let colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
 
   let compliment = (`Hey ${req.params.name},  ${(compliments[Math.floor(Math.random() * compliments.length)])}`)
   let color = colors[Math.floor(Math.random() * colors.length)]
@@ -50,4 +42,10 @@ app.get("/:name?", (req, res) => {
     compliment: compliment,
     color: color
   })
+})
+
+app.post('/', (req, res, next) => {
+  console.log(req.body)
+  compliments.push(req.body.user_compliment)
+  res.redirect('back')
 })
