7e861b74a6ab27770d8e322fab792fdb94ebf596
Meg MacIver <megmaciver13@gmail.com>
2017-12-05 14:14:38 -0500

    name appears

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const hbs = require('hbs')
+const bodyParser = require('body-parser')
 
 const app = express()
 
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 let compliments = [
   'Your instructors love you',
@@ -13,23 +15,27 @@ let compliments = [
   'The Force is strong with you'
 ]
 
+var capitalizeFirst = function (word) {
+  return word.charAt(0).toUpperCase() + word.slice(1)
+}
+
 app.get('/:name?', (req, res) => {
   let randomCompliment = compliments[Math.floor(Math.random() * compliments.length)]
-  let next = compliments[Math.floor(Math.random() * compliments.length)]
-  if (req.params.name) {
-    let name = req.params.name.charAt(0).toUpperCase() + req.params.name.slice(1)
-  }
+  let name = capitalizeFirst(req.params.name || '')
   let colors = ['#fda339', '#57d6d4', '#ff5f60', '#a7db48']
   let randomColor = colors[Math.floor(Math.random() * colors.length)]
+
   res.render('index', {
     randomCompliment,
-    next,
     colors,
     randomColor,
-    if (name) {
-      name
-    }
+    name
   })
+})
+
+app.post('/:name?', (req, res) => {
+  compliments.push(req.body.newCompliment)
+  res.redirect(`/${req.params.name}`)
 })
 
 app.listen(3000, () => {
