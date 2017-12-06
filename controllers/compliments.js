91608d21aa345b7974a21fa5acfae8bbddd4b1ae
Casey Jordan <Jordan_Casey.bah.com>
2017-12-06 14:39:50 -0500

    level 1

--- controllers/compliments.js
+++ controllers/compliments.js
@@ -2,13 +2,17 @@ const express = require('express')
 const Compliment = require('../db/schema')
 const router = express.Router()
 
+
 router.get('/', (req, res) => {
-  Compliment.find({})
-  .then((compliments) => {
-    res.render('compliment-show', {
-      compliments: compliments
+  var colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+  var num = [Math.floor(Math.random() * colors.length)]
+  Compliment.aggregate({ $sample: { size: 1 } })
+    .then((compliments) => {
+      res.render('compliment-show', {
+        compliment: compliments[0],
+        color: colors[num]
+      })
     })
-  })
 })
 
 
--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+
 mongoose.connect('mongodb://localhost/emergencycompliment', {useMongoClient: true})
 
 mongoose.Promise = Promise
