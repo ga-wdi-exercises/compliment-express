e69da9b8f1d546bd2396892445a606c1da942c22
Wade Stewart <stew.wade@gmail.com>
2017-12-19 13:48:44 -0500

    adding template

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-let compliments = [
-    "Your instructors love you",
-    "High five = ^5",
-    "Is it Ruby Tuesday yet?",
-    "It's almost beer o'clock",
-    "The Force is strong with you"
-  ]
-let colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+// var compliments = [
+//     "Your instructors love you",
+//     "High five = ^5",
+//     "Is it Ruby Tuesday yet?",
+//     "It's almost beer o'clock",
+//     "The Force is strong with you"
+//   ]
+// var colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
 const express = require('express')
+const hbs = require('express-handlebars')
+
 const app = express()
-
-app.set('view engine', 'hbs')
-
-app.get('/', (req, res) => {
- 
-    res.send('index', {compliments, colors})
-})
 
 app.listen(4000, () => {
     console.log('app listening')
 })
+
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+
