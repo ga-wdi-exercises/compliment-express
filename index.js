e35c3aa580b03ab326777e4e78e2c4776d190645
Justin Probst <justin.probst.09@cnu.edu>
2017-12-19 13:46:03 -0500

    stubbed express-handlebars config

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var hbs = require('express-handlebars');
 var app = express();
 
+// handlebars config
+app.set("view engine", "hbs")
+app.engine('.hbs', hbs({
+    extname:        '.hbs',
+    partialsDir:    'views/',
+    layoutsDir:     'views/',
+    defaultLayout:  'layout'
+}))
+  
+
+// Serve static content (css, etc) for the app from the “public” directory in the application directory
+app.use(express.static(__dirname + '/public')); 
+
 // establish bodyParser 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // random generator
 function generateRandom() {
@@ -14,7 +28,7 @@ function generateRandom() {
 // root route
 app.get("/", function(req, res) {
     generateRandom();
-    res.send('HELLO');
+    res.render("welcome");
 })
 
 // define the applications port
