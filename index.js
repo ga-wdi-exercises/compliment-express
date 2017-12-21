1172fbae1c0935214ea0670263910d9fcbff4b59
Ryan OKeefe <ryanokeefe208@gmail.com>
2017-12-21 10:33:29 -0500

    sending changes to index.js

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require("express")
 const app = express()
 const hbs = require("express-handlebars")
+const bodyParser = require("body-parser")
 
 app.set("view engine", "hbs")
 
+// handle json post reqs
+app.use(bodyParser.json())
+// handles form submissions
+app.use(bodyParser.urlencoded({ extended: true }))
 
+
+
+
+/////////////
 
 app.get("/:name", (req, res) => {
     res.send(`hello ${req.params.name}`)
 })
 
 app.get("/", (req, res) => {
-    res.send("hello world")
+    res.send("")
 })
 
 ////////////
