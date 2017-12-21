3bb438766edd452218026a8f14121194baf1fe76
Brian Reynolds <brikrey@gmail.com>
2017-12-20 21:42:56 -0500

    Level 3 functionality added

--- index.js
+++ index.js
@@ -1,57 +1,63 @@
 
 const express = require('express')
-const bodyParser = require('body-parser')
-const hbs        = require('hbs')
+const hbs     = require('hbs')
+const parser  = require('body-parser')
+
 const app = express()
-
-app.set('view engine', 'hbs')
-
-app.use(bodyParser.json()) // handles json post requests
-app.use(bodyParser.urlencoded({ extended: true })) // handles form submissions
 
 app.listen(4010, () => {
   console.log('app listening on port 4010')
 })
 
-complement = [
+app.set('view engine', 'hbs')
+
+app.use(parser.urlencoded({ extended: true }))
+app.use(parser.json())
+
+// compliment and color arrays
+let compliment = [
     "Your instructors love you",
     "Is it Ruby Tuesday yet?",
     "It's almost beer o'clock",
     "The Force is strong with you"
   ]
 
-colors = [
+let colors = [
     "#FFBF00",
     "#0080FF",
     "#01DF3A",
     "#FF0080"
   ]
 
+function getRandomInt(lnth) {
+  const min = 0
+  const max = lnth-1
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
 app.get('/', (req, res) => {
-  let compIndex = getRandomInt()
-  let comp = complement[compIndex]
-  let color = colors[compIndex]
+  let compIndex = getRandomInt(compliment.length)
+  let comp = compliment[compIndex]
+  let colorIndex = getRandomInt(colors.length - 1) 
+  let color = colors[colorIndex]
   res.render('index', {comp, color})
 })
 
-function getRandomInt() {
-    const min = 0
-    const max = 3
-    return Math.floor(Math.random() * (max - min + 1)) + min
-}
-
 app.get('/:visitor?', (req, res) => {
-  let compIndex = getRandomInt()
-  let comp = complement[compIndex]  
-  let color = colors[compIndex]
+  let compIndex = getRandomInt(compliment.length)
+  let comp = compliment[compIndex]
+  let colorIndex = getRandomInt(colors.length - 1) 
+  let color = colors[colorIndex]
   let visitor = req.params.visitor
   res.render('index', {comp, color, visitor})
 })
 
-// app.post('/', (req, res) => {
-//   res.render('index', {
-//     player_name: req.body.player_name,
-//     bottles: 99,
-//     next: 98
-//   })
-// })
+app.post('/', (req, res) => {
+  let visitor = req.params.visitor
+  compliment.push(req.body.newComp)
+  res.redirect(`/`)
+})
+
+app.listen(app.get('port'), () => {
+  console.log('It\'s aliiive!')
+})
