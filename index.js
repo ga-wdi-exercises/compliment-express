e9d30c48abd486c539e068ae1c9fb3cc73fcc675
Your Name <you@example.com>
2017-12-31 17:58:32 -0500

    can post comments to h3 tag

--- index.js
+++ index.js
@@ -8,43 +8,27 @@ app.set('view engine', 'hbs')
 app.use(parser.json())
 app.use(parser.urlencoded({ extended: true }))
 
-let compliments = [
-    "Your instructors love you",
-    "High five = ^5",
-    "Is it Ruby Tuesday yet?",
-    "It's almost beer o'clock",
-    "The Force is strong with you"
-  ]
-
 app.get('/', (req, res) => {
-    res.render('compliment-index')
+    let compliments = [
+        "Your instructors love you",
+        "High five = ^5",
+        "Is it Ruby Tuesday yet?",
+        "It's almost beer o'clock",
+        "The Force is strong with you"
+      ]
+    res.render('compliment-index', {compliments})
 })
 
 app.get('/:name', (req, res) => {
     let name = req.params.name
     res.render('compliment-index', { name })
-    // let greeting = `Hi ${name}`
-    // res.send(`${greeting} ${randomCompliement}`)
 })
 
 app.post('/', (req, res) => {
     res.render('compliment-index', {
-        name: req.body.compliment,
-
+        newCompliment: req.body.newCompliment,
     })
 })
-
-// app.get('/:name', (req, res) => {
-//     let name = req.params.name
-//     let greeting = `Hi ${name}`
-    
-//     for (i = 0; i < compliments.length; i++) {
-//         let random = (Math.floor(Math.random() * compliments.length))
-//         let randomCompliment = compliments[random]
-//         // let randomColor = colors[random]
-//         res.send(`${greeting} ${randomCompliment}`)
-//         }
-// })
 
 app.listen(4000, () => {
     console.log('yoooooooo')
