4685b94b8388160d6f5d8366af39df33b47fa0b2
Shelley Schoppert <schop23@gmail.com>
2017-12-31 20:26:32 -0500

    arrays

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const complimentController = require('./controllers/complimentController')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//use handlebars
+app.set('view engine', 'hbs')
 
 app.listen(4000, () => {
     console.log('app listening on port 4000')
 })
 
-app.get("/", (req, res) => {
-    res.send("Hello " + req.params.user_name)
-  })
-
-//use handlebars
-app.set('view engine', 'hbs')
+app.post('/', (req, res) => {
+  res.send(`Hello  + ${req.params.name}`)
+})
