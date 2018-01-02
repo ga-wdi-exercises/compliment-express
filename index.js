7be88684ba6d44b9aadbda125145d135791173ed
Christopher Tran <christopher.long.tran@gmail.com>
2018-01-02 00:45:26 -0500

    added more compliments from emergency compliments

--- index.js
+++ index.js
@@ -6,13 +6,19 @@ var compliments = [
   'High five = ^5',
   'Is it Ruby Tuesday yet?',
   "It's almost beer o'clock",
-  'The Force is strong with you'
+  'The Force is strong with you',
+  'People enjoy you accidentlally touching their butt while putting on your seat-belt',
+  'Your ex thought about you this morning, then they thought about donuts',
+  'Your principal would call you to the office just to look cool',
+  'Everyone at the laundromat thinks you have the cutest underwear',
+  'You rarely have to go to the bathroom when you fly in the window seat',
+  'You are freakishly good at thumb wars'
 ]
 
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  var index = Math.floor(Math.random() * 5)
+  var index = Math.floor(Math.random() * 11)
   var randCompliment = compliments[index]
   res.render('index', {randCompliment})
 })
