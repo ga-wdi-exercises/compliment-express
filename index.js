4cdb3c0abe7435c9096477ba8ce55e9da12f1a0a
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-02-16 22:41:18 -0500

    Level 1 complete

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+const compliments = [
+  'Your instructors love you',
+  'High five = ^5',
+  'Is it Ruby Tuesday yet?',
+  "It's almost beer o'clock",
+  'The Force is strong with you'
+]
+const randomNumber = function () {
+  return Math.floor(Math.random() * 5)
+}
+
+const colors = ['red', 'blue', 'green', 'gold', 'lime']
+
+const displayCompliment = compliments[randomNumber()]
+const displayColor = colors[randomNumber()]
+
+app.get('/', (req, res) => {
+  res.send(`${compliments[randomNumber()]} <body style=background-color:${colors[randomNumber()]}></body>`)
+})
+
+app.listen('4000', () => {
+  console.log('listening on port 4000')
+})
