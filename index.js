ff5c5b031629ef63bb2f0b7e04fb4555595b63f5
Mahwash Mahr <mahwash.mahr@gmail.com>
2018-02-23 09:04:47 -0500

    finished

--- index.js
+++ index.js
@@ -1,40 +1,41 @@
-const express = require("express")
+const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
-/* app.get("/:name", (req, res) => {
-    res.send(`hello ${req.params.name}`)
-  })
- */
-const compliments = [
-    "Your instructors love you",
-    "High five = ^5",
-    "Is it Ruby Tuesday yet?",
-    "It's almost beer o'clock",
-    "The Force is strong with you"
-  ]
-  
-const colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
-const randomNumber = function () {
-    return Math.floor(Math.random() * 5)
-  }
-const background = `background-color:${colors[randomNumber()]}`  
+app.set("view engine", "hbs")
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+var compliments = [
+   "Your instructors love you",
+   "High five = ^5",
+   "Is it Ruby Tuesday yet?",
+   "It's almost beer o'clock",
+   "The Force is strong with you"
+   ]
+
+var colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
 
 
 app.get('/', (req, res) => {
-    res.send(compliments[1])
+    var randomCompliments = compliments[Math.floor(Math.random() * compliments.length)]
+    var randomColor = colors[Math.floor(Math.random() * colors.length)]
+    res.render('index', {randomCompliments, randomColor})
 })
-  
 
+app.get('/:name', (req, res) => {
+     var randomCompliments = compliments[Math.floor(Math.random() * compliments.length)]
+     var name = req.params.name
+     res.render('index', {name, randomCompliments})
+})
 
-
-
-
-
-
-
-
+app.post('/', (req, res) => {
+    var newCompliments = req.body.newCompliments
+    console.log(req.body)
+    // var text = req.body.text
+    res.send(newCompliments)
+})
 
 app.listen(4000, () => {
-    console.log("app listening on port 4000")
-  })
-  
+    console.log('Connected 4000 port!')
